7632ea8232e178b38a406b4dc9ba77c4fbc5cc54
Vladimir <example@gmail.com>
2024-06-19 19:31:04 +0300

    completed  020(d) and 021(d) lessons

--- js/script.js
+++ js/script.js
@@ -397,4 +397,47 @@ const calc = (a, b) => {
 calc();
 */
 
-// ===== 020 (д) Еще раз про аргументы функций =====
+/* ===== 020 (д) Еще раз про аргументы функций =====
+
+const usdCurr = 28;
+const eurCurr = 32;
+
+function convert(amount, curr) {
+  console.log(curr * amount);
+}
+
+convert(500, usdCurr);
+convert(500, eurCurr);
+*/
+
+/* ===== 021 (д) Про важность return =====
+
+// const usdCurr = 28;
+// const discount = 0.9;
+
+// function convert(amount, curr) {
+//   return curr * amount;
+// }
+
+// function promotion(result) {
+//   console.log(result * discount);
+// }
+
+// const res = convert(500, usdCurr)
+// promotion(res);
+
+// function test() {
+//   for (let i = 0; i < 5; i++) {
+//     console.log(i);
+//     if (i === 3) return
+//   }
+//   console.log('Done')
+// }
+
+// test()
+
+function doNothing() { };
+console.log(doNothing === undefined);
+*/
+
+// ===== 022 Методы и свойства строк и чисел =====
