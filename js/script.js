29c637fc4d0863709df189d42d763c6cc0932c38
Vladimir <example@gmail.com>
2024-06-21 20:41:53 +0300

    completed 022 lesson

--- js/script.js
+++ js/script.js
@@ -440,4 +440,32 @@ function doNothing() { };
 console.log(doNothing === undefined);
 */
 
-// ===== 022 Методы и свойства строк и чисел =====
+/* ===== 022 Методы и свойства строк и чисел =====
+
+// const str = "test";
+// const arr = [1, 2, 3];
+
+// console.log(str[2] = 'd');
+
+// console.log(str.toUpperCase());
+// console.log(str.toLowerCase());
+// console.log(str);
+
+// const fruit = "Some fruit";
+// console.log(fruit.indexOf("q"));
+
+// const logg = "Hello world";
+// // console.log(logg.slice(-5, -1));
+// // console.log(logg.substring(6, 11));
+// console.log(logg.substr(6, 5));
+
+const num = 12.2;
+console.log(Math.round(num));
+
+const test = "12.2px"
+console.log(parseInt(test));
+console.log(parseFloat(test));
+*/
+
+// ===== 022 023 Практика , ч3. Используем функции =====
+
