75030bfbcb5a907f25da00bcd0fb8a49bc57cbe5
Vladimir <example@gmail.com>
2024-06-25 19:51:43 +0300

    completed 024(d) and 025 lessons

--- js/script.js
+++ js/script.js
@@ -469,3 +469,32 @@ console.log(parseFloat(test));
 
 // ===== 023 Практика , ч3. Используем функции =====
 
+// ===== 024 (д) Метод trim() =====
+
+// ===== 025 Callback- функции =====
+
+function first() {
+  // Do something
+  setTimeout(function () {
+    console.log(1);
+  }, 500);
+}
+
+function second() {
+  console.log(2);
+}
+
+first();
+second();
+
+function learnJS(lang, callback) {
+  console.log(`Я учу: ${lang}`);
+  callback();
+}
+
+function done() {
+  console.log('Я прошел этот урок!');
+}
+
+learnJS('JavaScript', done);
+
