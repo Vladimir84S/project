df031b99d8296b8d4fe5ac1274e481552ea8e97b
Vladimir <example@gmail.com>
2024-07-01 22:28:35 +0300

    completed 027 lesson

--- js/script.js
+++ js/script.js
@@ -548,6 +548,43 @@ console.log(border);
 // console.log(counter);
 */
 
-// ===== 027 Массивы и псевдомассивы =====
+/* ===== 027 Массивы и псевдомассивы =====
+
+const arr = [2, 13, 26, 8, 10]
+arr.sort(compareNum);
+console.log(arr);
 
 
+function compareNum(a, b) {
+  return a - b;
+}
+
+// arr[99] = 0;
+// console.log(arr.length);
+// console.log(arr);
+
+// arr.forEach(function (item, i, arr) {
+//   console.log(`${i}: ${item} внутри массива ${arr}`)
+// });
+
+// arr.pop();
+// arr.push(10);
+
+// console.log(arr);
+
+// for (let i = 0; i < arr.length; i++) {
+//   console.log(arr[i]);
+// }
+
+// for (let value of arr) {
+//   console.log(value);
+// }
+
+// const str = prompt("", "");
+// const products = str.split(", ");
+// products.sort();
+// console.log(products.join('; '));
+*/
+
+// ===== 028 урока небыло =====
+// ===== 029 Передача по ссылке или по значению, Spread оператор (ES6-ES9) =====
