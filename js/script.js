b5efa17a0dc98eb671d1ec053d46e48eb80a2d1f
Vladimir <example@gmail.com>
2024-07-10 21:39:07 +0300

    comleted 034 lesson

--- js/script.js
+++ js/script.js
@@ -798,4 +798,54 @@ console.log(typeof (Boolean('4')));
 console.log(typeof (!!"4444"));
 */
 
-// ===== 034 Замыкание и лексическое окружение =====
+/* ===== 034 Замыкание и лексическое окружение =====
+
+// let number = 5; debugger
+
+// function logNumber() {
+
+//   console.log(number); debugger
+// }
+
+// number = 6;
+
+// logNumber(); debugger
+
+// number = 8;
+
+// logNumber(); debugger
+
+function createCounter() {
+  let counter = 0;
+
+  const myFunction = function () {
+    debugger
+    counter = counter + 1; debugger
+    return counter; debugger
+  }
+
+  return myFunction;
+}
+debugger
+const increment = createCounter(); debugger
+const c1 = increment(); debugger
+const c2 = increment(); debugger
+const c3 = increment(); debugger
+
+console.log(c1, c2, c3);
+
+{
+  let msg = 'Hello'
+}
+
+console.log(msg)
+
+for (let i = 0; i < 9; i++) {
+  for (let j = 0; j < 9; j++) {
+    let num = 3;
+  }
+  console.log(num);
+}
+*/
+
+// ===== 035 Задачи с собеседований на понимание основ =====
