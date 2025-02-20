787dede7c1c5df3a9883186a8fbbd30c73e1c73c
unknown <Admin@CONSTRUKT-NOTE.ae.com>
2025-02-20 20:46:29 +0200

    completed 05/016 lesson

---
+++ 05 - Продвинутый JavaScript/016 Регулярные выражения/script.js
@@ -0,0 +1,33 @@
+'use strict';
+
+// new RegExp('pattern', 'flags');
+// /pattern/f
+
+// const ans = prompt('Ввведите ваше число');
+
+// const reg = /\d/g;
+// console.log(ans.match(reg));
+
+const str = 'My name is R2D2';
+
+console.log(str.match(/\D/gi));
+
+// \D
+// \W
+
+// \d
+// \w
+// \s
+
+// i
+// g
+// m
+
+// console.log(ans.search(reg));
+// console.log(ans.match(reg));
+
+// const pass = prompt('Password');
+
+// console.log(pass.replace(/./g, '*'));
+
+// console.log('12-34-56'.replace(/-/g, ':'));
