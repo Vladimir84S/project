16cfa300806c5a7ab24f7b00ebcc4cbace30c480
unknown <Admin@CONSTRUKT-NOTE.ae.com>
2025-03-04 21:17:05 +0200

    completed 05/020 lesson

--- 05 - Продвинутый JavaScript/019 Геттеры и сеттеры (свойства объектов)/script.js
+++ 05 - Продвинутый JavaScript/019 Геттеры и сеттеры (свойства объектов)/script.js
@@ -1,17 +1,90 @@
 'use strict';
 
-const persone = {
-  name: 'Alex',
-  age: 25,
+//  Инкапсуляция функции конструктора
 
-  get userAge() {
-    return this.age;
-  },
+/* function User(name, age) {
+//   this.name = name;
+//   let userAge = age;
 
-  set userAge(num) {
-    this.age = num;
-  },
-};
+//   this.say = function () {
+//     console.log(`Имя пользователя: ${this.name}, возраст ${userAge}`);
+//   };
 
-console.log((persone.userAge = 30));
-console.log(persone.userAge);
+//   this.getAge = function () {
+//     return userAge;
+//   };
+
+//   this.setAge = function (age) {
+//     if (typeof age === 'number' && age > 0 && age < 110) {
+//       userAge = age;
+//     } else {
+//       console.log('Недопустимое значение');
+//     }
+//   };
+// }
+
+// const ivan = new User('Ivan', 27);
+// console.log(ivan.name);
+// console.log(ivan.getAge());
+
+// ivan.setAge(30);
+// ivan.setAge(300);
+// console.log(ivan.getAge());
+
+//  ivan.say();
+*/
+
+//  Инкапсуляция классов
+
+class User {
+  constructor(name, age) {
+    this.name = name;
+    this._age = age;
+  }
+
+  #surname = 'Babay';
+
+  say = () => {
+    console.log(
+      `Имя пользователя: ${this.name}${this.#surname}, возраст ${this._age}`
+    );
+  };
+
+  get age() {
+    return this._age;
+  }
+
+  set age(age) {
+    if (typeof age === 'number' && age > 0 && age < 110) {
+      this._age = age;
+    } else {
+      console.log('Недопустимое значение');
+    }
+  }
+
+  // Getter для приватного свойства #surname
+
+  get surname() {
+    return this.#surname;
+  }
+
+  // Setter для приватного свойства #surname
+
+  set surname(value) {
+    if (typeof value === 'string' && value.trim().length > 0) {
+      this.#surname = value;
+    } else {
+      console.log('Недопустимое значение для фамилии');
+    }
+  }
+}
+
+const ivan = new User('Ivan', 27);
+// Получаем значение фамилии через getter
+console.log(ivan.surname); // Выведет: Babay
+// Устанавливаем новое значение фамилии через setter
+ivan.surname = 'Petrov';
+ivan.say(); // Выведет: Имя пользователя: Ivan Petrov, возраст 27
+// Попытка установить недопустимое значение
+ivan.surname = ''; // Выведет: Недопустимое значение для фамилии
+ivan.say();
