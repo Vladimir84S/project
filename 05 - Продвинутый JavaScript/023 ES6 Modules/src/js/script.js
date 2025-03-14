a1c76a7129b5b72482ba28ba4a9892dffdf23f4a
unknown <Admin@CONSTRUKT-NOTE.ae.com>
2025-03-14 19:20:39 +0200

    completed 05/023 lesson

---
+++ 05 - Продвинутый JavaScript/023 ES6 Modules/src/js/script.js
@@ -0,0 +1,18 @@
+// import { one, two } from './main';
+// import sayHi from './main';
+// import { one as first } from './main';
+
+// import * as data from './main';
+
+// console.log(`${one} and ${two}`);
+// sayHi();
+// console.log(first);
+
+// console.log(`${data.one} and ${data.two}`);
+// data.sayHi();
+
+import { one, two } from './main.js';
+import sayHi from './main.js';
+
+console.log(`${one} and ${two}`);
+sayHi();
