27b9d449491b95b3440faebe6289a3c8ff3d40c6
unknown <Admin@CONSTRUKT-NOTE.ae.com>
2025-04-11 21:21:32 +0300

    completed 06/006 lesson

--- 06 - Библиотека React. Базовый уровень/004 Create React App - создаем свое приложение/my-app/src/App.js
+++ 06 - Библиотека React. Базовый уровень/004 Create React App - создаем свое приложение/my-app/src/App.js
@@ -1,10 +1,51 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+// import React from 'react';
+import { Component } from 'react';
 import './App.css';
+
+const Header = () => {
+  return <h2>Hello world!</h2>;
+};
+
+// const Field = () => {
+//   const holder = 'Enter here';
+//   const styledField = {
+//     width: '300px',
+//   };
+//   return <input placeholder={holder} type="text" style={styledField} />;
+// };
+
+class Field extends Component {
+  render() {
+    const holder = 'Enter here';
+    const styledField = {
+      width: '300px',
+    };
+    return <input placeholder={holder} type="text" style={styledField} />;
+  }
+}
+
+function Btn() {
+  const text = 'Log in';
+  const logged = false;
+
+  // const res = () => {
+  //   return 'Log in';
+  // };
+  // const p = <p>Log in</p>;
+  // return <button>{3+4}</button>;
+
+  return <button>{logged ? 'Enter' : text}</button>;
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <Header />
+      <Field />
+      <Btn />
+
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,9 +58,10 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
 
+export { Header };
 export default App;
--- 06 - Библиотека React. Базовый уровень/004 Create React App - создаем свое приложение/my-app/src/index.js
+++ 06 - Библиотека React. Базовый уровень/004 Create React App - создаем свое приложение/my-app/src/index.js
@@ -2,19 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { Header } from './App';
 
 // const elem = <h2>Hello World!</h2>
 const text = 'Hello World!';
 
-const elem = (
-  <div>
-    {/* <h2>Hello World!</h2> */}
-    <h2 className="text">Текст: {text}</h2>
-    <input type="text" />
-    <label htmlFor=""></label>
-    <button tabIndex="0">Click</button>
-  </div>
-);
+// const elem = (
+//   <div>
+//     {/* <h2>Hello World!</h2> */}
+//     <h2 className="text">Текст: {text}</h2>
+//     <input type="text" />
+//     <label htmlFor=""></label>
+//     <button tabIndex="0">Click</button>
+//   </div>
+// );
 
 // const elem = React.createElement(
 //   'h2',
@@ -31,4 +32,4 @@ const elem = (
 // };
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(elem);
+root.render(<App />);
