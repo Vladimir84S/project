0187b2672daba41c3e576280ff5c59efa2c6d737
unknown <Admin@CONSTRUKT-NOTE.ae.com>
2025-04-24 20:07:25 +0300

    completed 06/011 lesson

--- 06 - Библиотека React. Базовый уровень/009 Создание нового проекта/src/components/app/app.js
+++ 06 - Библиотека React. Базовый уровень/009 Создание нового проекта/src/components/app/app.js
@@ -7,17 +7,22 @@ import EmployeesAddForm from '../employees-add-form/employees-add-form';
 import './app.css';
 
 function App() {
+  const data = [
+    { name: 'John C.', salary: 800, increase: false },
+    { name: 'Alex M.', salary: 3000, increase: true },
+    { name: 'Carl W.', salary: 5000, increase: false },
+  ];
   return (
     <div className="app">
-        <AppInfo />
+      <AppInfo />
 
-        <div className="search-panel">
-            <SearchPanel/>
-            <AppFilter/>
-        </div>
-        
-        <EmployeesList/>
-        <EmployeesAddForm/>
+      <div className="search-panel">
+        <SearchPanel />
+        <AppFilter />
+      </div>
+
+      <EmployeesList data={data} />
+      <EmployeesAddForm />
     </div>
   );
 }
